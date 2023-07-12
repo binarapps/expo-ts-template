cc5359bb9054dc69901375fe57ab8a591aa91487
Bartłomiej Sworzeń <bartlomiej.sworzen@binarapps.com>
2023-07-13 00:55:57 +0200

    feature/init test form screen

---
+++ src/screens/TestFormScreen.tsx
@@ -0,0 +1,9 @@
+import { Center, Text } from 'native-base'
+
+export const TestFormScreen = (): JSX.Element => {
+  return (
+    <Center flex={1}>
+      <Text textAlign="center">TestForm</Text>
+    </Center>
+  )
+}
